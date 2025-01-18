45911a373dbe2ab315d3c66b3978709bad9bfba2
Anuj Kumar Jha <net.anuj@gmail.com>
2025-01-18 12:56:30 +0530

    test

--- nurturenet.web.client/vite.config.ts
+++ nurturenet.web.client/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 49907,
-    },
-    build: {
-        outDir: 'build',
-       
-    }
+  plugins: [react()],
+  server: {
+    port: 49907,
+  },
+  build: {
+    outDir: 'build',
+  },
 });
