7ecf11537f179117b73f0edbf78f1466da41f386
Anuj Kumar Jha <net.anuj@gmail.com>
2025-01-18 12:25:34 +0530

    ff test

--- nurturenet.web.client/scripts/copyConfig.js
+++ nurturenet.web.client/scripts/copyConfig.js
@@ -8,7 +8,9 @@ const __dirname = path.dirname(__filename);
 
 const buildDir = path.join(__dirname, '../build');
 const configFile = path.join(__dirname, '../staticwebapp.config.json');
-const destFile = path.join(buildDir, 'staticwebapp.config.json');
+const headersFile = path.join(__dirname, '../public/_headers');
+const destConfigFile = path.join(buildDir, 'staticwebapp.config.json');
+const destHeadersFile = path.join(buildDir, '_headers');
 
 // Ensure the build directory exists
 if (!fs.existsSync(buildDir)) {
@@ -16,6 +18,13 @@ if (!fs.existsSync(buildDir)) {
 }
 
 // Copy the config file to the build directory
-fs.copyFileSync(configFile, destFile);
-
+fs.copyFileSync(configFile, destConfigFile);
 console.log('staticwebapp.config.json copied to build directory');
+
+// Copy the headers file to the build directory
+if (fs.existsSync(headersFile)) {
+  fs.copyFileSync(headersFile, destHeadersFile);
+  console.log('_headers file copied to build directory');
+} else {
+  console.warn('_headers file not found');
+}
